0313fa7c5911174f4cb4f1b15a69300ddeea4664
Michal <misiek-pl@o2.pl>
2019-09-10 13:11:09 +0200

    added js from to make username

--- src/main/java/bom/user/UserContextService.java
+++ src/main/java/bom/user/UserContextService.java
@@ -1,6 +1,7 @@
 package bom.user;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,8 +11,11 @@ import org.springframework.web.context.annotation.SessionScope;
 @Service
 @SessionScope
 public class UserContextService {
+    @Autowired
+    private UsersRepository<User> usersRepository;
 
     private static Gson gson = new Gson();
+    private User user;
 
     public String getLoggedUserEmail() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -20,7 +24,6 @@ public class UserContextService {
         }
         return authentication.getName();
     }
-
 
     public boolean admin() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
@@ -38,6 +41,14 @@ public class UserContextService {
         return false;
     }
 
+    public String getUserFirstName(){
+        return usersRepository.findByUsername(getLoggedUserEmail()).get().getFirstName();
+    }
+
+    public String getUserSurName(){
+        return usersRepository.findByUsername(getLoggedUserEmail()).get().getSurName();
+
+    }
 
     public void getCurrentBomUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
